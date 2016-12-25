f4a85a0e5b63f970408949873d6a341cb7d0fac0
Sutthinart Khunvadhana <iakgoog@gmail.com>
2016-12-26 04:33:37 +1100

    Initial Commit

---
+++ Proof of Concept/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Proof of Concept
+//
+//  Created by Sutthinart Khunvadhana on 26/12/16.
+//  Copyright © 2016 Sutthinart Khunvadhana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
